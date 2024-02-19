bb2225b717af485b8ff0a6452c3d13345e4f538f
muboxgithub <160022284+muboxgithub@users.noreply.github.com>
2024-02-19 08:41:38 +0300

    Update firstpage.php
    last

--- firstpage.php
+++ firstpage.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -22,20 +23,59 @@ $result=$conn->query($sql);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Seletion domain Site</title>
+  <title>Select student for domain</title>
  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.css" />
-  
+ 
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
 
 <style>
 
+  @media screen and (max-width:768px)
+
+  {
+   
+.responsive-table{
+ overflow-x:auto;
+  white-space:nowrap;
+  display:block;
+}
+.responsive-table td,
+.responsive-table th
+{
+  display:inline-block;
+  vertical-align:top;
+  min-width: 200px;
+
+}
 
 
-    .body{
+.responsive-table td::before,
+.responsive-table th::before {
+  content:attr(data-table);
+  font-weight:bold;
+  margin-bottom:0.5em;
+
+}
+
+
+}
+
+body
+{
+   /* overflow:hidden;*/
+}
+.form-group{
+ // margin-right:10px;
+}
+
+    body{
         background-color: #f8f8f8;
     }
     #we
@@ -55,23 +95,49 @@ margin-bottom:10px;
    .selectedroo td{
   background-color: #ffeeba; /* Change the color as desired */
 }
-  
+  .text-right
+  {
+    //text-align:right;
+  }
+
+  select option:hover
+
+  {
+    background-color:#9932cc;
+    border-color: #ced4da;
+  }
+  #rr{
+box-shadow:0 0 4px rgba(0,0,0,0.2);
+background-color:white;
+  }
 </style>
 </head>
 <body>
 
 
-    <div class="container mt-3">
+    <div  id="rr" class="container mt-3">
    
     <div class="row">
 
-
-    <div class="col-sm-4">
+    <div class="col mt-3">
     <div class="form-group">
-    <select id="gender" class="form-control">
-<option value="">Select by gender</option>
-<option value="male">Male</option>
-<option value="female">Female</option>
+    <select id="school" class="form-select">
+<option value="">School</option>
+<option value="School1">School1</option>
+<option value="school2">school2</option>
+
+
+    </select>
+
+</div>
+</div>
+
+    <div class="col mt-3">
+    <div class="form-group">
+    <select id="gender" class="form-select">
+<option value="">Gender</option>
+<option value="Male">Male</option>
+<option value="Female">Female</option>
 
 
     </select>
@@ -81,18 +147,28 @@ margin-bottom:10px;
 
 
 
-<div class="col-sm-4">
+<div class="col mt-3">
 <div class="form-group">
 
-    <select id="region" class="form-control">
-        <option value="">Select by Region</option>
-        <option value="addisabeba">A.A</option>
-        <option value="amahra">Amhara</option>
-        <option value="Tigraie">Tigraie</option>
-        <option value="oromoia">Oromoia</option>
-        <option value="afar">Afar</option>
-        <option value="somalia">Somale</option>
-        <option value="other">Other</option>
+    <select id="firstselector" class="form-select">
+        <option value="">Region</option>
+        <option value="AddisAbaba">A.A</option>
+        <option value="Afar">Afar</option>
+        <option value="Amhara">Amhara</option>
+        <option value="BenishangulGumuz">Benishangul-Gumuz</option>
+        <option value="CentralEthiopia">Central-Ethiopia</option>
+        <option value="DireDawa">Dire Dawa</option>
+        <option value="Gambela">Gambela</option>
+        <option value="Harari">Harari</option>
+        <option value="Oromia">Oromia</option>
+        <option value="Sidama">Sidama</option>
+        <option value="Somali">Somali</option>
+        <option value="SouthEthiopia"> SouthEthiopia</option>
+        <option value="SouthwestEthiopia">South west Ethiopia</option>
+        <option value="Tigray">Tigray</option>
+
+
+       
 
 </select>
 
@@ -100,15 +176,11 @@ margin-bottom:10px;
 </div>
 
 
-<div class="col-sm-4">
+<div class="col mt-3">
 <div class="form-group">
-<select id="grade" class="form-control">
-    <option value="">Select by Grade</option>
-    <option value="12">G12</option>
-    <option value="11">G11</option>
-    <option value="10">G10</option>
-    <option value="9">G9</option>
-    <option value="8">G8</option>
+<select id="secondselctor" class="form-select">
+    <option value="">Zone</option>
+   
 </select>
 
 </div>
@@ -119,13 +191,17 @@ margin-bottom:10px;
 
 </div>
 
-<div class="container align-left mt-2">
+<div  class="container align-left mt-2">
     
 
-<div class="mb-3 mt-3">
+<div class="d-flex mt-3">
 
-
-<input id="randomselect" type="input" class="form-control" placeholder="input numbers to select randomly">
+<div class="pl-3">
+<input id="userinput" type="input" class="form-control" placeholder="input numbers">
+</div>
+<div class="container pl-3 ml-3">
+<button  id="randomselect" type="button" class="btn btn-outline-primary">Select Random</button>
+</div>
 
 </div>
 
@@ -144,7 +220,7 @@ margin-bottom:10px;
 <div class="container mt-2 text-success text-center">
 <p id="Totalsum"></p>
 </div>
-<table id="example" class="display" style="width:100%">
+<table id="example" class="responsive-table" style="width:100%;">
     
 
 
@@ -159,17 +235,16 @@ margin-bottom:10px;
 
 
 
-            <th>Name</th>
+            <th data-table="Name">Name</th>
           
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Grade </th>
-            <th>Cost</th>
-            <th>School</th>
-            <th>Gender</th>
-            <th>Region</th>
-            <th>Zone</th>
-            <th>City</th>
+            <th data-table="Email">Email</th>
+            <th data-table="Phone">Phone</th>
+            <th data-table="Grade">Grade </th>          
+            <th data-table="School">School</th>
+            <th data-table="Gender">Gender</th>
+            <th data-table="Region">Region</th>
+            <th data-table="Zone">Zone</th>
+            <th data-table="City">City</th>
         </tr>
     </thead>  
     <tbody>
@@ -191,7 +266,8 @@ if($result->num_rows >0){
 
             <?php
 
-            $cost=$row['grade'];
+
+$cost=$row['grade'];
 if($row['grade']== 'G12')
 {
 $cost=10;
@@ -210,7 +286,7 @@ else{
 }
 ?>
 
-            <td><?php echo $cost ?></td>
+            
 
 
             <td><?php  echo $row['school'] ?></td>
@@ -231,24 +307,167 @@ else{
 }
 ?>
     </tbody>
-    <tfoot>
-        <tr>
-        <th>Name</th>
-         
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Grade </th>
-            <th>Cost</th>
-            <th>School</th>
-            <th>Gender</th>
-            <th>Region</th>
-            <th>Zone</th>
-            <th>City</th>
-        </tr>
-    </tfoot>
+    
 </table>
 
+<div class="container mt-1 text-right">
+    
+    <button type="button" id="submitbutton" class="btn btn-success" style="background-color:#9932cc;">Submit</button>
+
+
 </div>
+
+
+<div class="modal" id="Mymodal">
+
+
+<div class="modal-dialog">
+
+
+<div class="modal-content">
+
+
+
+
+
+<div class="modal-header">
+
+<h4 class="modal-title">
+Notification
+</h4>
+
+<button type="button" class="btn-close" data-bs-dismiss="modal">
+
+
+</button>
+
+
+</div>
+
+<div class="modal-body">
+
+<div class="container border mb-3">
+<p id="summerytext"></p>
+</div>
+<form action="">
+
+<div class="form-group mt-2">
+  <label for="inputName"><span style="color:#9932cc;font-size:20px;"><i class="fa fa-user"></i>&nbsp;</span>User Name</label>
+<input type="text" placeholder="Enter your name" class="form-control mt-2" id="username" required>
+
+</div>
+<div class="form-group mt-2">
+<label for="inputName"><span style="color:#9932cc;font-size:20px;" required><i class="fa fa-envelope"></i>&nbsp;</span>Email</label>
+<input type="email" id="emailvalid" placeholder="Enter your email" class="form-control mt-2">
+</div>
+<div class="form-group mt-2">
+<label for="inputName"><span style="color:#9932cc;font-size:20px;"><i class="fa fa-address-card-o"></i>&nbsp;</span>Address</label>
+
+<input type="text" placeholder="Enter your address" class="form-control">
+<div>
+
+<div class="form-group mt-2">
+<label for="inputName"><span style="color:#9932cc;font-size:20px;" required><i class="fa fa-phone"></i>&nbsp;</span>Phone</label>
+
+<input type="number" id="phonecount" placeholder="Enter your phone number" class="form-control">
+</div>
+<div class="form-group mt-3 mb-3">
+<button  data-bs-toggle="modal" data-bs-target="#modaljj" class="btn btn-primary">Submit</button>
+</div>
+</form>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="modal-footer">
+
+<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+</div>
+
+
+
+
+
+</div>
+
+</div>
+
+
+</div>
+
+
+<div class="modal" id="modaljj">
+
+<div class="modal-dialog">
+
+
+
+<div class="modal-content">
+
+
+<div class="modal-header">
+
+<h4 class="modal-title">hh</h4>
+<button type="button" class="btn-close" data-bs-dismiss="modal">
+</div>
+
+<div class="modal-content">
+
+<p>safas</p>
+</div>
+
+
+<div class="modal-footer">
+<button type="button"  class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+</div>
+
+
+
+</div>
+
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
     <script type="text/javascript">
         new DataTable('#example');
 $(document).ready(function(){
@@ -265,34 +484,55 @@ $(document).ready(function()
 
             var table= $('#example').DataTable();
             var SelectedRow = [];
+            var totalCost=0;
 
             $('#gender').on('change',function()
             {
 
             var selectgender=$(this).val();
-            table.column(6).search(selectgender).draw();
+
+            if(selectgender === 'Male'){
+                table.column(5).search('^' + selectgender + '$',true,false).draw();
+
+            }
+            else{
+                table.column(5).search(selectgender).draw();
+            }
+           
             });
 
 
+
+            //slect by shool
+
+            $('#school').on('change',function(){
+   var school=$(this).val();
+
+
+   table.column(4).search(school).draw();
+
+
+            });
+
 //slection for region
-            $('#region').on('change',function()
+            $('#firstselector').on('change',function()
             {
 
             var reg=$(this).val();
 
 
-            table.column(7).search(reg).draw();
+            table.column(6).search(reg).draw();
 
             });
 
             //for selection buy grade 
 
-            $('#grade').on('change',function()
+            $('#secondselctor').on('change',function()
             {
 
             var grade=$(this).val();
 
-            table.column(4).search(grade).draw();
+            table.column(7).search(grade).draw();
             });
 
 
@@ -325,7 +565,7 @@ for (var i= 0;  i < SelectedRow.length; i++)
 
     var rowData=table.row(rowIndex).data();
 
-    var cost=parseFloat(rowData[4]);
+    var cost=500;
 
 
     totalCost += cost;
@@ -341,8 +581,125 @@ $('#Totalsum').text('TotalSelectedCost is:' + totalCost);
     }
   });
 
+$('#submitbutton').on('click',function(){
+var SelectedCount=SelectedRow.length;
+
+var totalCost=0;
+
+for(var i= 0; i < SelectedRow.length; i++){
+
+
+var rowIndex=SelectedRow[i];
+
+var rowData=table.row(rowIndex).data();
+
+var cost=500;
+
+totalCost += cost;
+
+}
+
+var summeryText= 'Total Number of Selected: ' + SelectedCount +'&nbsp; Student' +'<br> Total cost is: ' + totalCost + 'Birr';
+
+document.getElementById('summerytext').innerHTML=summeryText;
+
+
+var modal=new bootstrap.Modal(document.getElementById('Mymodal'));
+
+if(totalCost > 0){
+modal.show();
+}
+else{
+  window.alert('please select to submit');
+}
+
+modal._element.addEventListener('hidden.bs.modal',function()
+{
+
+  $('.modal-backdrop').remove();
+});
+
+});
+
+
             
 
+
+  $('#randomselect').on('click',function(){
+
+
+    table.$('tr.selectedroo').removeClass('selectedroo');
+
+
+    var userInput= parseInt($('#userinput').val());
+
+
+    var visiblerow=table.rows({search:'applied', page:'current'}).nodes();
+
+var selectedRow=$(visiblerow).slice(0,userInput);
+
+$(selectedRow).addClass('selectedroo');
+
+
+var selectedCount=$(table.rows('.selectedroo').nodes()).length;
+
+$('#selectedcount').text('Total numeber of selected count is:'+ selectedCount);
+
+
+var totalCost=0;
+var fixedcost=500;
+
+$(selectedRow).each(function(){
+
+
+
+
+totalCost +=fixedcost;
+});
+
+$('#Totalsum').text('Total amount of random select cost is: ' + totalCost);
+
+
+
+  });
+
+
+
+
+  $('#submitbutton').on('click',function(){
+
+
+    var SelectedCount=$(table.rows('.selectedroo').nodes()).length;
+
+
+    var totalCost=SelectedCount *500;
+
+    var SelectedRow=table.rows('.selectedroo').data().toArray();
+
+
+
+
+    var summeryText= 'Total Number of Selected: ' + SelectedCount +'&nbsp; Student' +'<br> Total cost is: ' + totalCost + 'Birr';
+
+document.getElementById('summerytext').innerHTML=summeryText;
+
+
+var modal=new bootstrap.Modal(document.getElementById('Mymodal'));
+
+
+if(totalCost > 0){
+modal.show();
+}
+
+
+modal._element.addEventListener('hidden.bs.modal',function(){
+
+
+  $('.modal-backdrop').remove();
+});
+
+  });
+/*
 
   $('#randomselect').on('click',function()
   {
@@ -364,14 +721,108 @@ var SelectedCou=$(table.rows('.selectedroo').nodes()).length;
 $('#selectedcount').text('Total number of Random Selected:' + SelectedCou)
 
   });
-
+*/
  
             });
 
 
+            const options = {
+              AddisAbaba: [],
+    Afar: ["Awsi Rasu", "Kilbet Rasu", "Gabi Rasu", "Fanti Rasu", "Hari Rasu", "Mahi Rasu (New Zone)", "Argobba (special woreda)"],
+    Amhara: ["Agew Awi", "East Gojjam", "North Gondar", "Central Gondar", "West Gondar", "Wag Hemra", "West Gojjam", "Bahir Dar (special zone)", "West Gojjam", "South Gondar", "North Wollo", "South Wollo", "Oromia", "North Shewa"],
+    BenishangulGumuz: ["Asosa", "Kamashi", "Metekel"],
+    CentralEthiopia: ["East Gurage Zone", "Gurage Zone", "Halaba Zone", "Hadiya Zone", "Kebena Special Woreda", "Kembata Zone", "Mareko Special Woreda", "Silt'e Zone", "Tembaro Special Woreda", "Yem Zone"],
+    DireDawa: [],
+    Gambela: ["Anywaa", "Majang", "Nuer"],
+    Harari: ["Amir-Nur Woreda", "Abadir Woreda", "Shenkor Woreda", "Jin'Eala Woreda", "Aboker Woreda", "Hakim Woreda", "Sofi Woreda", "Erer Woreda", "Dire-Teyara Woreda"],
+    Oromia: ["East Arsi", "West Arsi", "East Bale", "West Bale", "Borana", "East Hararghe", "East Shewa", "East Welega", "East Guji", "West Guji", "Horo Guduru Welega", "Illubabor", "Buno Bedele", "Jimma", "Kelam Welega", "North Shewa", "Southwest Shewa", "West Haraghe", "West Shewa", "West Welega", "Oromia Special Zone Surrounding Finfinne"],
+    Sidama: ["Central Sidama Zone", "Eastern Sidama Zone", "Northern Sidama Zone", "Southern Sidama Zone"],
+    Somali: ["Sitti Zone", "Fafan Zone", "Jarar Zone", "Erer Zone", "Nogob Zone", "Dollo Zone", "Korahe Zone", "Shabelle Zone", "Afder Zone", "Liben Zone", "Dhawa Zone", "Jigjiga Special Zone", "Tog Wajale Special Zone", "Degehabur Special Zone", "Gode Special Zone", "Kebri Beyah Special Zone", "Kebri Dahar Special Zone"],
+    SouthEthiopia: ["Ale Zone", "Amaro Zone", "Ari Zone", "Basketo Zone", "Burji Zone", "Dirashe Zone", "Gamo Zone", "Gedeo Zone", "Gofa Zone", "Konso Zone", "South Omo Zone", "Wolayita Zone"],
+    SouthwestEthiopia: ["Bench Sheko Zone", "Dawro Zone", "Keffa Zone", "Sheka Zone", "Konta Zone", "West Omo Zone"],
+    Tigray: ["Central Tigray", "East Tigray", "North West Tigray", "South Tigray", "South East Tigray", "West Tigray", "Mekele (special zone)"],
+};
+
+const firstSelector = document.getElementById('firstselector');
+const secondSelector = document.getElementById('secondselctor');
+
+firstSelector.addEventListener("change", function() {
+  const selectedGroup = this.value;
+
+  secondSelector.innerHTML = '<option value="">Select option</option>';
+
+  if (selectedGroup && options[selectedGroup]) {
+    options[selectedGroup].forEach(function(option) {
+      const optionElement = document.createElement("option");
+      optionElement.value = option;
+      optionElement.textContent = option;
+      secondSelector.appendChild(optionElement);
+    });
+  }
+});
+
+
+var userInput=document.getElementById('username');
+
+
+userInput.addEventListener('change',function(){
+
+
+var use=userInput.value;
+var regex = /^[a-zA-Z]+$/;
+
+if(!regex.test(use)){
+
+userInput.setCustomValidity('only letters are allowed');
+}
+else{
+  userInput.setCustomValidity('');
+}
+});
 
 
 
+var emailInput=document.getElementById('emailvalid');
+
+
+
+emailInput.addEventListener('change',function(){
+  var em=emailInput.value;
+var regex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if(!regex.test(em)){
+  emailInput.setCustomValidity('Please Enter valid format email');
+}
+else{
+  emailInput.setCustomValidity('');
+}
+
+
+});
+
+
+
+var phoneInput=document.getElementById('phonecount');
+
+
+phoneInput.addEventListener('change',function(){
+
+
+
+var phonein=this.value;
+
+var regex=/^\d{10}$/;
+
+if(!regex.test(phonein)){
+
+  phoneInput.setCustomValidity('Please Enter Corrrect Phon neumber');
+}
+else{
+  phoneInput.setCustomValidity('');
+}
+
+
+})
     </script>
 </div>
 </body>
